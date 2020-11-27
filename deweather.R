debd3c5e664787681563b5376388a55e33de4fca
Hubert Thieriot <hubert.thieriot@gmail.com>
2020-11-27 00:45:27 +0000

    - adding manual deweather results

--- deweather.R
+++ deweather.R
@@ -2,9 +2,19 @@ source('./config.R')
 
 if(T){
   require(creadeweather)
-  m.dew <- creadeweather::deweather(city=city, source="defra", poll="no2",
+  m.dew.20200316 <- creadeweather::deweather(city=city, source="defra",
+                                             poll=c("no2","pm25"),
                                     training_start_anomaly ="2017-03-16",
                                     training_end_anomaly = "2020-03-16",
                                     upload_results = F)
+  saveRDS(m.dew.20200316, "resuts/m.dew.20200316.RDS")
+
+  m.dew.20200316.pbl <- creadeweather::deweather(city=city, source="defra",
+                                                 poll=c("no2","pm25"),
+                                    training_start_anomaly ="2017-03-16",
+                                    training_end_anomaly = "2020-03-16",
+                                    years_force_refresh = NULL,
+                                    upload_results = F)
+  saveRDS(m.dew.20200316.pbl, "resuts/m.dew.20200316.pbl.RDS")
 
 }
